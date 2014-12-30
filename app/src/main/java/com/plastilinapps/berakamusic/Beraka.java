a03c7220e9ffb806a017813bf24a3095fb570573
zekeApps <sheqel@live.com>
2014-12-29 23:51:13 -0600

    New activity added dimens added for values-v21 (Lollipop)

--- app/src/main/java/com/plastilinapps/berakamusic/Beraka.java
+++ app/src/main/java/com/plastilinapps/berakamusic/Beraka.java
@@ -1,9 +1,15 @@
 package com.plastilinapps.berakamusic;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Outline;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class Beraka extends Activity {
@@ -12,28 +18,24 @@ public class Beraka extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beraka);
+
+
+        ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                // Or read size directly from the view's width/height
+                int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
+                outline.setOval(0, 0, size, size);
+            }
+        };
+        ImageButton imageButton = (ImageButton) findViewById(R.id.loginButton);
+        imageButton.setOutlineProvider(viewOutlineProvider);
+
     }
+    public void onClick(View v){
+        Toast.makeText(this, "I am listening", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, Music.class);
+        startActivity(intent);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_beraka, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
