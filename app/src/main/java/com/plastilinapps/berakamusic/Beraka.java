895afd3ab768f0a078bae48ba5fa94f270832db1
zekeApps <sheqel@live.com>
2014-12-30 18:19:46 -0600

    Very basic Fragments added

--- app/src/main/java/com/plastilinapps/berakamusic/Beraka.java
+++ app/src/main/java/com/plastilinapps/berakamusic/Beraka.java
@@ -2,13 +2,11 @@ package com.plastilinapps.berakamusic;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Outline;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.view.View;
-import android.view.ViewOutlineProvider;
-import android.widget.ImageButton;
+
 import android.widget.Toast;
 
 
@@ -18,6 +16,8 @@ public class Beraka extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beraka);
+        /*
+        ImageButton imageButton = (ImageButton) findViewById(R.id.loginButton);
 
 
         ViewOutlineProvider viewOutlineProvider = new ViewOutlineProvider() {
@@ -28,12 +28,13 @@ public class Beraka extends Activity {
                 outline.setOval(0, 0, size, size);
             }
         };
-        ImageButton imageButton = (ImageButton) findViewById(R.id.loginButton);
+
         imageButton.setOutlineProvider(viewOutlineProvider);
+        */
 
     }
     public void onClick(View v){
-        Toast.makeText(this, "I am listening", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Welcome to Beraka Music Store", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, Music.class);
         startActivity(intent);
 
--- app/src/main/java/com/plastilinapps/berakamusic/Music.java
+++ app/src/main/java/com/plastilinapps/berakamusic/Music.java
@@ -1,39 +1,41 @@
 package com.plastilinapps.berakamusic;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class Music extends Activity {
+
+public class Music extends Activity implements ActionBar.TabListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        setTabs();
+    }
+    private void setTabs(){
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(actionBar.newTab().setText("TopList").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Friends").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Profile").setTabListener(this));
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_music, menu);
-        return true;
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 }
